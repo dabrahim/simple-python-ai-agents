a123893cca6d85016cb3d33dd58d2b1d915fbbab
Ibrahima NDIAYE <ibrahima.ndiaye450@gmail.com>
2025-07-19 07:40:21 +0000

    Initial commit

---
+++ main.py
@@ -0,0 +1,14 @@
+from dotenv import load_dotenv
+from openai import OpenAI
+import os
+
+load_dotenv()
+api_key = os.getenv('OPENAI_API_KEY')
+
+client = OpenAI()
+response = client.responses.create(
+    model="gpt-4.1",
+    input="Write a one-sentence bedtime story about a unicorn."
+)
+
+print(response.output_text)
