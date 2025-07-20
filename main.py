425854af9d7c67211250c047edd6a25fb82fea72
Ibrahima NDIAYE <ibrahima.ndiaye450@gmail.com>
2025-07-20 13:21:07 +0000

    Research notes added

--- main.py
+++ main.py
@@ -2,13 +2,33 @@ from dotenv import load_dotenv
 from openai import OpenAI
 import os
 
+from openai.types.chat import ChatCompletionSystemMessageParam, ChatCompletionUserMessageParam
+
 load_dotenv()
 api_key = os.getenv('OPENAI_API_KEY')
 
 client = OpenAI()
-response = client.responses.create(
-    model="gpt-4.1",
-    input="Write a one-sentence bedtime story about a unicorn."
-)
 
-print(response.output_text)
+
+def get_completion(chat_messages: list) -> str:
+    completion = client.chat.completions.create(
+        model="gpt-4.1",
+        messages=chat_messages,
+    )
+    return completion.choices[0].message.content
+
+
+messages = [
+    ChatCompletionSystemMessageParam(
+        content="You you are a helpful assistant named Jeff made by The Agent Forge.",
+        role="system"
+    ),
+    ChatCompletionUserMessageParam(
+        role="user",
+        content="Who are you?",
+    )
+]
+
+print(
+    get_completion(messages)
+)
---
+++ system-prompt.md
@@ -0,0 +1,8 @@
+**Key sections**
+* Identity / Role / Goal
+* Capabilities — The things it can do / You excel at the following tasks:
+* Mode of operation : Agent Loop + events — plan, action (tool call), observation (tool response)
+* High level planning steps — How to break down tasks / How to tackle problems
+* Tools : definition (separate file)
+* Specific rules for each tool category
+* Response format : JSON only (Structured output vs Prompt) / Tool call only
